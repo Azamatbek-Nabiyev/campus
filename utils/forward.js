bbe0832fd6fd981a86e2817608f89d6ae85db032
NabiyevDeveloper <azamatnabiyev77@gmail.com>
2025-07-29 15:21:44 +0500

    fixed 2

--- utils/forward.js
+++ utils/forward.js
@@ -1,19 +1,25 @@
 const { CHANNEL_ID } = require('../config');
 
+let messageCounter = 1;
+
 function approveMessage(ctx, message) {
+  const suffix = `\n\n#${messageCounter++}`; // tartib raqami qo‘shiladi
+
   if (message.text) {
-    return ctx.telegram.sendMessage(CHANNEL_ID, message.text);
+    return ctx.telegram.sendMessage(CHANNEL_ID, message.text + suffix);
   } else if (message.photo) {
     const largestPhoto = message.photo[message.photo.length - 1];
+    const caption = (message.caption || '') + suffix;
     return ctx.telegram.sendPhoto(CHANNEL_ID, largestPhoto.file_id, {
-      caption: message.caption || '',
+      caption,
     });
   } else if (message.video) {
+    const caption = (message.caption || '') + suffix;
     return ctx.telegram.sendVideo(CHANNEL_ID, message.video.file_id, {
-      caption: message.caption || '',
+      caption,
     });
   } else {
-    return ctx.reply('This file is not acceptable');
+    return ctx.reply('This file type is not supported for posting.');
   }
 }
 
